caebe7f78280312d3c262a7132c0daeb13d2d8ed
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-12 11:07:40 +0100

    zadanie38

---
+++ main.js
@@ -0,0 +1,16 @@
+let f = function (a, b) {
+    let add == function add(a+b) ;
+    return console.log("Wynik dodawania wynosi f(add)")
+
+
+    let subtract == function substract (a - b);
+    let mn == function cotu?    (a * b;);
+
+    return console.log("Wynik dodawania wynosi f(add)");
+    return console.log("Wynik odejmowania wynosi f(subtract)");
+    return console.log("Wynik mnożenia wynosi f(mn)");
+
+    if (add || subtract || ?? <0)
+    return console.log("Wynik jest nieprawidłowy");
+
+}
